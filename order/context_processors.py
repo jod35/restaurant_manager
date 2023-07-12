1912e6c606c071b2506e7880b6519397685bb043
ruthrina <ruth@gmail.com>
2023-07-12 13:06:49 +0100

    resp

--- order/context_processors.py
+++ order/context_processors.py
@@ -5,10 +5,10 @@ from datetime import datetime
 
 User = get_user_model()
 
-username = f'anon_{datetime.now().timestamp()}'
-test_user = User.objects.create_user(username=username, password=None, last_name='last', first_name='')
+# username = f'anon_{datetime.now().timestamp()}'
+# test_user = User.objects.create_user(username=username, password=None, last_name='last', first_name='')
 
-order = Order.objects.create(user=User.objects.get(id=1))
+# order = Order.objects.create(user=User.objects.get(id=1))
 
 
 def order_processor(request):
