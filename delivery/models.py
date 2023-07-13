f77aedd8ea15e6dfc30afb8b8da0ce4bf4876619
jod35 <jodestrevin@gmail.com>
2023-07-13 06:21:52 +0300

    remove delivery model

--- delivery/models.py
+++ delivery/models.py
@@ -5,18 +5,3 @@ from order.models import Order
 
 User = get_user_model()
 
-# Create your models here.
-
-class Delivery(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    address = models.CharField(max_length=255)
-    delivered = models.BooleanField(default=False)
-    delivery_date = models.DateField()
-    delivery_time = models.TimeField()
-
-    def __str__(self):
-        return f'Delivery for {self.order} at {self.address}'
-    
-    class Meta:
-        verbose_name_plural = "Deliveries"
