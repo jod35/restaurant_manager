7cb640b92a2ef2d7225233d11de56926e81ee4f1
ruthrina <ruth@gmail.com>
2023-07-19 15:30:55 +0100

    changes

--- restaurantproj/settings.py
+++ restaurantproj/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # STATICFILES_DIRS = (BASE_DIR / 'static',)
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Default primary key field type
