b42adf09aa09277a9d65209a6540c7ff7b5678dc
ruthrina <ruth@gmail.com>
2023-07-19 15:12:34 +0100

    deploy

--- restaurantproj/settings.py
+++ restaurantproj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-9jv=)wjyjnawy!#_!khctsu-##**x!(mjmvs18sbyi6cv!n6ip
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
